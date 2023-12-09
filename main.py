104ab680324b6cf4a95d6344615435671a2dfb5b
Bluesadsilk <bluesadsilk@proton.me>
2023-12-09 16:42:14 +0100

    cosas

--- main.py
+++ main.py
@@ -10,37 +10,37 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ Devuelve la página principal de Inicio de la web"""
-    return render_template('index.html')
+    return render_template('index.html', current_page='index')
 
 
 @app.route('/actividades')
 def actividades():
     """Devuelve la página de actividades de la web"""
-    return render_template('actividades.html')
+    return render_template('actividades.html', current_page='actividades')
 
 
 @app.route('/gastronomia')
 def gastronomia():
     """Devuelve la página de gastronomía de la web"""
-    return render_template('gastronomia.html')
+    return render_template('gastronomia.html', current_page='gastronomia')
 
 
 @app.route('/cultura')
 def cultura():
     """Devuelve la página de cultura de la web"""
-    return render_template('cultura.html')
+    return render_template('cultura.html', current_page='cultura')
 
 
 @app.route('/idioma')
 def idioma():
     """Devuelve la página de idioma de la web"""
-    return render_template('idioma.html')
+    return render_template('idioma.html', current_page='idioma')
 
 
 @app.route('/contacto')
 def contacto():
     """Devuelve la página de contacto de la web"""
-    return render_template('contacto.html')
+    return render_template('contacto.html', current_page='contacto')
 
 
 if __name__ == '__main__':
